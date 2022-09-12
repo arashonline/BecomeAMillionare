d478544849c7892022af4fdf5a234af3d80c0d62
arashadmin <arash.rabiee.88@gmail.com>
2022-09-12 14:19:45 +0430

    adding SiteController

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::namespace('App\Http\Controllers')->group(function () {
+    Route::get('/', 'SiteController@index');
+    Route::get('/about', 'SiteController@about');
 });
+
